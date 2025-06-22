1d208db92b25dca53e77d06c5c2c2338eb13f1bd
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2025-06-22 10:42:57 -0300

    add updated design for the login page

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,69 +1,132 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import classes from "./Login.module.css";
-import escudo from "../../Img/escudo.png";
+import AuthService from "../../services/authService";
+
 function Login() {
+  // Mensaje de error en la tarjeta
+  const [err_msg, setErrorMsg] = useState("");
+  const [isHidden, setHidden] = useState(true);
+
+  //para poder ver la contraseña
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  // Estado para almacenar el nombre de usuario (en este caso CI) y password
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Función que se llama cuando se envía el formulario
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (username === "1111") {
-      navigate("/HomeMiembroMesa");
-    } else if (username === "2222") {
-      navigate("/HomeAdministrador");
-    } else {
-      alert("Usuario o clave incorrectos.");
+    // Validación de campos vacíos
+    if (!formData.username || !formData.password) {
+      setErrorMsg("Los campos no pueden estar vacíos.");
+      setHidden(false);
+      return;
+    }
+
+    setErrorMsg("");
+    setHidden(true);
+
+    try {
+      const user = await AuthService.login(
+        formData.username,
+        formData.password
+      );
+
+      console.log("Respuesta del servidor:", user);
+
+      if (user.code === 200) {
+        console.log("Respuesta del servidor:", user);
+        console.log("datos guardados", user.data.user_data);
+        
+        // Redirigir según el rol
+        if (user.data.role_description === "admin") {
+            console.log("Redirigiendo a HomeAdministrador");
+          navigate("/HomeAdministrador");
+        } else if (user.data.role_description === "miembroMesa") {
+          navigate("/HomeMiembroMesa");
+        } else {
+          setErrorMsg("Rol no válido. Contacta al administrador.");
+          setHidden(false);
+        }
+      }
+    } catch (error) {
+      setErrorMsg("Ocurrió un error durante el inicio de sesión.");
+      setHidden(false);
+      console.error("Login error:", error);
     }
   };
 
   return (
     <div className={classes.loginContainer}>
       <div className={classes.loginWrapper}>
-        <img src={escudo} className={classes.escudo} />
-
+        <div>
+          <img
+            src="../../../public/Escudo20Uruguay_19.png"
+            alt="Logo"
+            className={classes.logo}
+          />
+        </div>
         <h2 className={classes.loginTitle}>Inicio de Sesión</h2>
-        <p className={classes.loginSubtitle}>
-          Inicia sesión con tu usuario de la <strong>corte electoral</strong>.
+        <p>
+          Inicia sesión tu usuario de la <b>corte electoral</b>.
         </p>
 
         <form onSubmit={handleSubmit} className={classes.loginForm}>
           <div className={classes.inputGroup}>
-            <label className={classes.inputLabel}>Nombre de usuario</label>
+            <p className={classes.inputLabel}>Nombre de usuario</p>
             <input
               className={classes.loginInput}
               type="text"
-              placeholder="Ingrese su nombre de usuario"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
               required
+              placeholder="Ingrese su nombre de usuario"
             />
           </div>
-
           <div className={classes.inputGroup}>
-            <label className={classes.inputLabel}>Contraseña</label>
+            <p className={classes.inputLabel}>Contraseña</p>
             <input
               className={classes.loginInput}
               type={showPassword ? "text" : "password"}
               placeholder="Contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
               required
             />
-            <div className={classes.checkboxContainer}>
+            <div className={classes.showPassword}>
               <input
                 type="checkbox"
-                id="mostrar"
+                id="show-password"
+                name="password"
                 checked={showPassword}
-                onChange={() => setShowPassword(!showPassword)}
+                onChange={togglePasswordVisibility}
               />
-              <label htmlFor="mostrar">Mostrar contraseña</label>
+              <label htmlFor="show-password">Mostrar contraseña</label>
             </div>
           </div>
-
           <button className={classes.loginButton} type="submit">
             Iniciar sesión
           </button>
