731a7b140ae9768f42df740be216b29754632432
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2025-07-09 14:48:00 -0300

    fix voto en blanco y anulado

--- frontend-votante/src/Context/FlujoContext.jsx
+++ frontend-votante/src/Context/FlujoContext.jsx
@@ -3,98 +3,95 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const FlujoContext = createContext();
 
 const flujo = [
-    { tipo: "inicio" },
-    { tipo: "municipal" },
-    { tipo: "resumen" },
-    { tipo: "confirmacion" },
+  { tipo: "inicio" },
+  { tipo: "municipal" },
+  { tipo: "resumen" },
+  { tipo: "confirmacion" },
 ];
 
-
 export function FlujoProvider({ children }) {
-    const [etapaActual, setEtapaActual] = useState(0);
-    const [respuestas, setRespuestas] = useState([]);
+  const [etapaActual, setEtapaActual] = useState(0);
+  const [respuestas, setRespuestas] = useState([]);
 
-    const etapa = flujo[etapaActual];
+  const etapa = flujo[etapaActual];
 
-    // cambiar
-    const guardarVoto = (tipo, opcion) => {
-        setRespuestas(//(prev) => [
-      //  ...prev.filter((r) => r.tipo !== tipo),
-        { tipo, opcion }
-        );
-    };
+  const guardarVoto = ({ tipo, opcion }) => {
+    setRespuestas((prev) => [
+      ...prev.filter((r) => r.tipo !== tipo),
+      { tipo, opcion },
+    ]);
+  };
 
-    const reset = () => {
-        setEtapaActual(0);
-        setRespuestas([]);
-        localStorage.removeItem("etapaActual");
-        localStorage.removeItem("ci_ciudadano");
-    };
+  const reset = () => {
+    setEtapaActual(0);
+    setRespuestas([]);
+    localStorage.removeItem("etapaActual");
+    localStorage.removeItem("ci_ciudadano");
+  };
 
-    useEffect(() => {
-        const saved = localStorage.getItem("etapaActual");
-        if (saved !== null) {
-            setEtapaActual(parseInt(saved));
-        }
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem("etapaActual", etapaActual);
-        console.log("Etapa actual guardada:", etapaActual);
-    }, [etapaActual]);
-
-    const limpiarRespuestas = () => {
-        setRespuestas([]);
-    };
-
-    const siguiente = () => {
-        let nextStep = null;
-        if (etapaActual + 1 < flujo.length) {
-            nextStep = flujo[etapaActual + 1];
-        }
-        setEtapaActual((prev) => {
-            if (prev + 1 < flujo.length) {
-                nextStep = flujo[prev + 1];
-                return prev + 1;
-            }
-            return prev;
-        });
-        return nextStep;
-    };
-
-
-    const anterior = () => {
-        let prevStep = flujo[0];
-        setEtapaActual((prev) => {
-            if (prev > 0) {
-                prevStep = flujo[prev - 1];
-                return prev - 1;
-            }
-            return prev;
-        });
-
-        return prevStep;
-    };
-
-    return (
-        <FlujoContext.Provider
-        value={{
-            etapa,
-            etapaActual,
-            flujo,
-            guardarVoto,
-            siguiente,
-            anterior,
-            reset,
-            respuestas,
-            limpiarRespuestas
-        }}
-        >
-        {children}
-        </FlujoContext.Provider>
-    );
+  useEffect(() => {
+    const saved = localStorage.getItem("etapaActual");
+    if (saved !== null) {
+      setEtapaActual(parseInt(saved));
     }
+  }, []);
 
-    export function useFlujo() {
-    return useContext(FlujoContext);
+  useEffect(() => {
+    localStorage.setItem("etapaActual", etapaActual);
+    console.log("Etapa actual guardada:", etapaActual);
+  }, [etapaActual]);
+
+  const limpiarRespuestas = () => {
+    setRespuestas([]);
+  };
+
+  const siguiente = () => {
+    let nextStep = null;
+    if (etapaActual + 1 < flujo.length) {
+      nextStep = flujo[etapaActual + 1];
+    }
+    setEtapaActual((prev) => {
+      if (prev + 1 < flujo.length) {
+        nextStep = flujo[prev + 1];
+        return prev + 1;
+      }
+      return prev;
+    });
+    return nextStep;
+  };
+
+  const anterior = () => {
+    let prevStep = flujo[0];
+    setEtapaActual((prev) => {
+      if (prev > 0) {
+        prevStep = flujo[prev - 1];
+        return prev - 1;
+      }
+      return prev;
+    });
+
+    return prevStep;
+  };
+
+  return (
+    <FlujoContext.Provider
+      value={{
+        etapa,
+        etapaActual,
+        flujo,
+        guardarVoto,
+        siguiente,
+        anterior,
+        reset,
+        respuestas,
+        limpiarRespuestas,
+      }}
+    >
+      {children}
+    </FlujoContext.Provider>
+  );
+}
+
+export function useFlujo() {
+  return useContext(FlujoContext);
 }
--- frontend-votante/src/Pages/Resumen.jsx
+++ frontend-votante/src/Pages/Resumen.jsx
@@ -9,74 +9,129 @@ export default function Resumen() {
   console.log("Resumen renderizado", respuestas);
   const [respuesta, setRespuesta] = useState([]);
 
+  // const renderOpcion = (tipo, opcion) => {
+  //   console.log("renderOpcion recibido:", tipo);
+  //   tipo = respuestas.tipo.tipo
+  //   opcion = respuestas.tipo.opcion
+
+  //   console.log("Es un objeto válido", opcion);
+  //   if (opcion) {
+
+  //     return (
+  //       <div>
+  //         <p>
+  //           <strong>Lista:</strong> {opcion.nro}
+  //         </p>
+  //         <p>
+  //           <strong>Candidato:</strong> {opcion.candidato}
+  //         </p>
+  //         <p>
+  //           <strong>Partido:</strong> {opcion.partido}
+  //         </p>
+  //       </div>
+  //     );
+
+  //     if ("descripcion" in opcion && "respuesta" in opcion) {
+  //       console.log("Es una consulta");
+  //       const traduccion =
+  //         {
+  //           votoLista: "Sí",
+  //           votoBlanco: "Voto en Blanco",
+  //           votoAnulado: "Voto Anulado",
+  //         }[opcion.respuesta] || opcion.respuesta;
+
+  //       return (
+  //         <div>
+  //           <p>{traduccion}</p>
+  //         </div>
+  //       );
+  //     }
+  //   }
+
+  //   console.log("No se pudo interpretar la opción", opcion);
+  //   return (
+  //     <p>
+  //       <strong>Respuesta:</strong> {String(opcion)}
+  //     </p>
+  //   );
+  // };
+
+  // useEffect(() => {
+  //   let id_estado = 1;
+  //   let es_observado = localStorage.getItem("es_observado");
+  //   let nro_circuito = localStorage.getItem("nro_circuito");
+  //   let id_papeleta = localStorage.getItem("id_papeleta");
+  //   let current_respuesta = [
+  //     { id_estado, es_observado, nro_circuito, id_papeleta },
+  //   ];
+  //   setRespuesta(current_respuesta);
+  // }, []);
+
   const renderOpcion = (tipo, opcion) => {
-    console.log("renderOpcion recibido:", tipo);
-    tipo = respuestas.tipo.tipo
-    opcion = respuestas.tipo.opcion
-
-    console.log("Es un objeto válido", opcion);
-    if (opcion) {
-
+    if (!opcion)
       return (
-        <div>
-          <p>
-            <strong>Lista:</strong> {opcion.nro}
-          </p>
-          <p>
-            <strong>Candidato:</strong> {opcion.candidato}
-          </p>
-          <p>
-            <strong>Partido:</strong> {opcion.partido}
-          </p>
-        </div>
+        <p>
+          <strong>Respuesta:</strong> No definida
+        </p>
       );
 
-      if ("descripcion" in opcion && "respuesta" in opcion) {
-        console.log("Es una consulta");
-        const traduccion =
-          {
-            votoLista: "Sí",
-            votoBlanco: "Voto en Blanco",
-            votoAnulado: "Voto Anulado",
-          }[opcion.respuesta] || opcion.respuesta;
+    if ("respuesta" in opcion) {
+      const traduccion =
+        {
+          votoLista: "Sí",
+          votoBlanco: "Voto en Blanco",
+          votoAnulado: "Voto Anulado",
+        }[opcion.respuesta] || opcion.respuesta;
 
-        return (
-          <div>
-            <p>{traduccion}</p>
-          </div>
-        );
-      }
+      return <p>{traduccion}</p>;
     }
 
-    console.log("No se pudo interpretar la opción", opcion);
     return (
-      <p>
-        <strong>Respuesta:</strong> {String(opcion)}
-      </p>
+      <div>
+        <p>
+          <strong>Lista:</strong> {opcion.nro}
+        </p>
+        <p>
+          <strong>Candidato:</strong> {opcion.candidato}
+        </p>
+        <p>
+          <strong>Partido:</strong> {opcion.partido}
+        </p>
+      </div>
     );
   };
 
   useEffect(() => {
-    let id_estado = 1;
-    let es_observado = localStorage.getItem("es_observado");
-    let nro_circuito = localStorage.getItem("nro_circuito");
-    let id_papeleta = localStorage.getItem("id_papeleta");
-    let current_respuesta = [
-      { id_estado, es_observado, nro_circuito, id_papeleta },
-    ];
-    setRespuesta(current_respuesta);
-  }, []);
+    const es_observado = parseInt(localStorage.getItem("es_observado") || "0");
+    const nro_circuito = parseInt(localStorage.getItem("nro_circuito"));
+
+    const votos = respuestas.map((r) => {
+      let id_estado = 1;
+      if (r.opcion.respuesta === "votoBlanco") id_estado = 2;
+      else if (r.opcion.respuesta === "votoAnulado") id_estado = 3;
+
+      return {
+        id_estado,
+        es_observado,
+        nro_circuito,
+        id_papeleta: r.opcion.id_papeleta,
+      };
+    });
+
+    setRespuesta(votos);
+  }, [respuestas]);
 
   const hayVotoAnulado = false;
 
   const handleSiguienteClick = async () => {
     console.log("Confirmando votación...");
-    console.log(respuesta)
+    console.log(respuesta);
     await votarService.emitirVoto(respuesta);
     // await votarService.emitirVoto(respuestas);
     navigate("/confirmacion");
   };
-  console.log("putas respuestas: ", respuestas)
+  console.log("putas respuestas: ", respuestas);
+  
   return (
     <div className="section">
       <h1 className="title is-2">Resumen de tu voto</h1>
@@ -86,9 +141,11 @@ export default function Resumen() {
             <p className="title is-3">Voto Anulado</p>
           </div>
         ) : (
-          respuesta.map((r, index) => (
+          respuestas.map((r, index) => (
             <div key={`${r.tipo}-${index}`} className="mb-5">
-              <h2 className="title is-4 has-text-link">{r.tipo}</h2>
+              <h2 className="title is-4 has-text-link">
+                {formatearTitulo(r.tipo)}
+              </h2>
               {renderOpcion(r.tipo, r.opcion)}
               <hr />
             </div>
